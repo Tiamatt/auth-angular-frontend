f88113a92eef625b649509b7934d118da8ac192a
tiamatt <samira.yusifova@gmail.com>
2019-05-04 10:51:26 -0400

    Design login page

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+// custom objects
+
 
 @Component({
   selector: 'app-login',
@@ -6,10 +9,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  fg_login: FormGroup;
 
   constructor() { }
 
+  private setForm(): void {
+    this.fg_login = new FormGroup({
+      'fc_email': new FormControl('', [
+        Validators.required,
+        Validators.email,
+      ]),
+    });
+  }
+
+  onLoginViaFacebook() {
+
+  }
+
+  onLoginViaInstagram() {
+
+  }
+
+  onLoginViaGoogle() {
+
+  }
+
+  onLoginViaEmail() {
+
+  }
+
   ngOnInit() {
+    this.setForm();
   }
 
 }
